2e2ee7dee285fd1ac1c54602518076d174c98df7
Abel Puentes Luberta <apuentes@abalt.org>
2016-06-27 11:11:13 +0200

    Added view to return Swagger to API Gateway Amazon

--- rest_framework_swagger/urls.py
+++ rest_framework_swagger/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from rest_framework_swagger.views import SwaggerResourcesView, SwaggerApiView, SwaggerUIView
+from rest_framework_swagger.views import SwaggerResourcesView, SwaggerApiView, SwaggerUIView, AWSSwaggerAPIView
 
 
 urlpatterns = [
     url(r'^$', SwaggerUIView.as_view(), name="django.swagger.base.view"),
     url(r'^api-docs/$', SwaggerResourcesView.as_view(), name="django.swagger.resources.view"),
     url(r'^api-docs/(?P<path>.*)/?$', SwaggerApiView.as_view(), name='django.swagger.api.view'),
+    url(r'^api-docs-aws/$', AWSSwaggerAPIView.as_view(), name='django.swagger.api.aws.view'),
 ]
